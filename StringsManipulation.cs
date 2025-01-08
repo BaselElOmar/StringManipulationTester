c789f93ff4a8b97ee71c474e0c09fc097a079414
OlliCupitt <ollicupitt1999@gmail.com>
2025-01-08 11:34:54 +0100

    first method added

--- StringsManipulation.cs
+++ StringsManipulation.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using Xunit;
 
 namespace CiCdgrupp
 
 public class StringsManipulation
 {
-	public StringsManipulation()
-	{
-	}
+
+    string originString = "Hello World";
+
+
+    public string TUpper(string origingstring)
+    {
+        origingstring = origingstring.ToUpper();
+        return originString;
+    }
+
+
+
 }
